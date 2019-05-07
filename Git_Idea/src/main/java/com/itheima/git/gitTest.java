d2759da0ef8b5d91f9bcbfc47f2a3575ae43bee4
chengwei <444712273@qq.com>
2019-05-07 16:38:39 +0800

    git上传测试

---
+++ Git_Idea/src/main/java/com/itheima/git/gitTest.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
